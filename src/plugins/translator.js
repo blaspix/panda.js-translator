4d5b66d1518e3c4c38e681f6ad36ae8eab276257
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2014-03-06 19:57:53 +0200

    Added support for 1.1.4

--- src/plugins/translator.js
+++ src/plugins/translator.js
@@ -1,73 +1,67 @@
 game.module(
-        'plugins.translator'
-    )
-    .require(
-        'engine.core'
-    )
-    .body(function () {
+    'plugins.translator'
+)
+.body(function () {
 
-        game.Translator = game.Class.extend({
+game.Translator = game.Class.extend({
+    messages: null,
+    fallback: 'en',
 
-            init: function () {
-                this.language = pandaConfig.translatorLanguage || navigator.userLanguage || navigator.language;
-                this.fallback = pandaConfig.translatorFallback || 'en';
-                var loader = new PIXI.JsonLoader('media/messages.json', true);
-                var scope = this;
-                loader.on('loaded', function() {
-                    scope.messages = loader.json;
-                });
-                loader.load();
-            },
+    init: function () {
+        game.addAsset(game.Translator.file);
+        game.trans = game.translate = this.translate.bind(this);
+    },
 
-            translate: function (message, params) {
-                if (typeof message == 'function') {
-                    message = message();
+    translate: function (message, params) {
+        if(!this.messages) this.messages = game.getJSON(game.Translator.file);
+
+        var messageLanguage, messageBlock, messageFallback, blocks, i;
+        if(typeof message === 'function') {
+            message = message();
+        }
+        if(typeof this.messages[game.Translator.lang] === 'object' ) {
+            if(typeof this.messages[game.Translator.lang][message] === 'string' ) {
+                messageLanguage = this.messages[game.Translator.lang][message];
+            } else {
+                messageBlock = this.messages[game.Translator.lang];
+                blocks = message.split('.');
+                for(i = 0; i < blocks.length; i++) {
+                    messageBlock = messageBlock[blocks[i]];
                 }
-                if(typeof this.messages[this.language] == 'object' ) {
-                    if(typeof this.messages[this.language][message] == 'string' ) {
-                        var messageLanguage = this.messages[this.language][message];
-                    } else {
-                        var messageBlock = this.messages[this.language];
-                        var blocks = message.split('.');
-                        for(var i = 0; i < blocks.length; i++) {
-                            messageBlock = messageBlock[blocks[i]];
-                        }
-                        if(typeof messageBlock == 'string') {
-                            var messageLanguage = messageBlock;
-                        }
-                    }
+                if(typeof messageBlock === 'string') {
+                    messageLanguage = messageBlock;
                 }
-                if (typeof messageLanguage != 'string') {
-                    if(typeof this.messages[this.fallback] == 'object' ) {
-                        if(typeof this.messages[this.fallback][message] == 'string' ) {
-                            var messageFallback = this.messages[this.fallback][message];
-                        } else {
-                            var messageBlock = this.messages[this.fallback];
-                            var blocks = message.split('.');
-                            for(var i = 0; i < blocks.length; i++) {
-                                messageBlock = messageBlock[blocks[i]];
-                            }
-                            if(typeof messageBlock == 'string') {
-                                var messageFallback = messageBlock;
-                            }
-                        }
-                    }
-                }
-                message = messageLanguage || messageFallback || message;
-                if(typeof params == 'object') {
-                    for(var name in params) {
-                        message = message.replace('%' + name + '%', params[name]);
-                    }
-                }
-                return message;
             }
+        }
+        if(typeof messageLanguage !== 'string') {
+            if(typeof this.messages[this.fallback] === 'object' ) {
+                if(typeof this.messages[this.fallback][message] === 'string' ) {
+                    messageFallback = this.messages[this.fallback][message];
+                } else {
+                    messageBlock = this.messages[this.fallback];
+                    blocks = message.split('.');
+                    for(i = 0; i < blocks.length; i++) {
+                        messageBlock = messageBlock[blocks[i]];
+                    }
+                    if(typeof messageBlock === 'string') {
+                        messageFallback = messageBlock;
+                    }
+                }
+            }
+        }
+        message = messageLanguage || messageFallback || message;
+        if(typeof params === 'object') {
+            for(var name in params) {
+                message = message.replace('%' + name + '%', params[name]);
+            }
+        }
+        return message;
+    }
+});
 
-        });
+game.Translator.lang = navigator.userLanguage || navigator.language;
+game.Translator.file = 'media/lang.json';
 
-        game.Translator = new game.Translator;
+game.plugins.translator = game.Translator;
 
-        game.trans = function (message, params) {
-            return game.Translator.translate(message, params);
-        };
-
-    });
+});
